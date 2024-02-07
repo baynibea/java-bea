88343880deb4c6ca57735a5dce3b5f331c241f58
baynibea <158351681+baynibea@users.noreply.github.com>
2024-02-07 16:41:59 -0400

    Add files via upload

---
+++ Beap.java
@@ -0,0 +1,17 @@
+package beap;
+import javax.swing.JOptionPane;
+
+public class beap
+{
+    public static void main( String[] args )
+    {
+        String firstNumber = 
+                JOptionPane.showInputDialog( "Enter first integer");
+        String secondNumber = 
+                JOptionPane.showInputDialog("Enter second integer");
+        
+        int number1 = Integer.parseInt( firstNumber );
+        int number2 = Integer.parseInt( secondNumber );
+        int sum = number1 + number2;
+    }
+}
